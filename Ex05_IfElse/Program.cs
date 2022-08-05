4b5e50e8f68f9775ac5d8df2728af6df1f87100e
Mary <maryqueenk1@gmail.com>
2022-08-05 20:02:03 +0300

    Написали особое приветствие для маши

---
+++ Ex05_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Ура, Это же Маша!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
